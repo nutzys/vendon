0074dca57976d9112b1aa57fdd02cd2241f51e44
User <User@DESKTOP-R07R23M>
2022-11-24 13:48:01 +0200

    Output question/answer

--- app/view/pages/test.php
+++ app/view/pages/test.php
@@ -7,9 +7,14 @@
     <title>Tests</title>
 </head>
 <body>
+
     <h1><?php echo $data['question'][$data['obj_num']]->text?></h1>
     <form action="<?php echo URLROOT?>/pages/test/<?php echo $data['test']->test_id?>" method="post">
         <input type="hidden" name="secret" value="<?php echo $data['obj_num']?>">
+        <input type="hidden" name="secret_question" value="<?php echo $data['secret_num']?>">
+        <?php foreach($data['answers'] as $answer):?>
+            <?php echo $answer->name?>
+        <?php endforeach;?>
         <button>Next</button>
     </form>
 </body>
