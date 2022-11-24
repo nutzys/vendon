1ed212f45f5a3d056c6e1f1f2c12bd9294b5b6b6
User <User@DESKTOP-R07R23M>
2022-11-24 13:47:07 +0200

    Answer query

--- app/model/Page.php
+++ app/model/Page.php
@@ -8,12 +8,14 @@ class Page
         $this->db = new Database();
     }
 
-    public function getTest(){
+    //Get test name
+    public function getTestName(){
         $this->db->query('SELECT * FROM test');
         $rows = $this->db->all();
         return $rows;
     }
 
+    //Get test by id
     public function getTestById($id){
         $this->db->query('SELECT * FROM test WHERE test_id = :id');
         $this->db->bind(':id', $id);
@@ -21,11 +23,20 @@ class Page
         return $row;
     }
 
-    public function getQuestion($testId){
-        //Most confusing query i've ever wrote 
+    //Get test info
+    public function getTest($id){
         $this->db->query('SELECT t.test_id, t.name, q.test_id, q.question_id, q.text, q.answer_id FROM test AS t INNER JOIN questions AS q ON t.test_id = q.test_id WHERE t.test_id = :id AND q.test_id = :id ORDER BY question_id ASC');
-        $this->db->bind(':id', $testId);
+        $this->db->bind(':id', $id);
         $rows = $this->db->all();
         return $rows;
     }
+
+    public function getAnswers($questionId, $id){
+        $this->db->query('SELECT name, question_id FROM answers WHERE question_id = :question AND test_id = :id');
+        $this->db->bind(':question', $questionId);
+        $this->db->bind(':id', $id);
+        $rows = $this->db->all();
+        return $rows;
+    }
+
 }
