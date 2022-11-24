501081810777bc67a6f170a579e786bf8de02621
User <User@DESKTOP-R07R23M>
2022-11-24 17:00:55 +0200

    Score display added

---
+++ app/view/pages/score.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beigas</title>
+</head>
+<body>
+    <h1>Tests pabeigts, Paldies, <?php echo $_SESSION['name']?></h1>
+    <p>Iegūtie punkti - <?php echo $_SESSION['score']?> no <?php echo $_SESSION['max_score']?></p>
+</body>
+</html>
