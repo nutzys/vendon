accaf8528b1a77706fb7bbf4de9907ab4e6e3207
User <User@DESKTOP-R07R23M>
2022-11-24 17:00:32 +0200

    Score display added

--- app/view/pages/test.php
+++ app/view/pages/test.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo URLROOT?>/css/test.css">
     <title>Tests</title>
 </head>
 <body>
@@ -12,8 +13,9 @@
     <form action="<?php echo URLROOT?>/pages/test/<?php echo $data['test']->test_id?>" method="post">
         <input type="hidden" name="secret" value="<?php echo $data['obj_num']?>">
         <input type="hidden" name="secret_question" value="<?php echo $data['secret_num']?>">
-        <?php foreach($data['answers'] as $answer):?>
-            <?php echo $answer->name?>
+        <input type="hidden" name="score" value="<?php echo $data['score']?>">
+        <?php foreach($data['answers'] as $answer):?>       
+            <input type="radio" name="input" class="btn" value="<?php echo $answer->answer_id?>" required><?php echo $answer->name . $answer->answer_id?>
         <?php endforeach;?>
         <button>Next</button>
     </form>
